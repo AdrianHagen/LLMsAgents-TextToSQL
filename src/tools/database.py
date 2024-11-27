f56af8e3138a25803213527032797217a2bc0477
Yannik Hahn <yannik@h4hn.de>
2024-11-27 18:52:48 +0100

    fix: database connection

--- src/tools/database.py
+++ src/tools/database.py
@@ -9,7 +9,7 @@ def list_databases() -> list:
     db_directory = os.getenv("DB_DIRECTORY")
     for root, dirs, files in os.walk(db_directory):
         for file in files:
-            if file.endswith('.sqlite'):
+            if file.endswith(".sqlite"):
                 databases.append(os.path.join(root, file))
     db_list = []
     for db in databases:
@@ -17,27 +17,17 @@ def list_databases() -> list:
         db_list.append({"name": db_name, "path": db})
     return db_list
 
-class SingletonMeta(type):
-    _instances = {}
-    _lock: Lock = Lock()
 
-    def __call__(cls, *args, **kwargs):
-        with cls._lock:
-            if cls not in cls._instances:
-                instance = super().__call__(*args, **kwargs)
-                cls._instances[cls] = instance
-        return cls._instances[cls]
-
-class Database(metaclass=SingletonMeta):
+class Database:
     def __init__(self, name: str):
         self.name = name
-        self.path = self._get_db_path(name)
+        self.path = self.get_db_path()
         self.connection = None
 
-    def _get_db_path(self, name: str) -> str:
+    def get_db_path(self) -> str:
         databases = list_databases()
         for db in databases:
-            if db["name"] == name:
+            if db["name"] == self.name:
                 return db["path"]
         raise ValueError(f"Database with name {name} not found.")
 
@@ -53,5 +43,3 @@ class Database(metaclass=SingletonMeta):
         results = cursor.fetchall()
         cursor.close()
         return results
-        
-
